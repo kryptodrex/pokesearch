7b79855db3ea3caa8972fc4c3b3ff557d71ac731
Joseph Lunine <jel99@drexel.edu>
2019-08-05 22:19:14 -0400

    Initial push

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
+    <Header />
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  cursor: url('assets/images/logo.svg'), auto !important;
+}
+
+/* Font Declaration */
+@font-face {
+    font-family: 'QSBold';
+    src: url('assets/fonts/quicksand-bold-webfont.woff2') format('woff2'),
+         url('assets/fonts/quicksand-bold-webfont.woff') format('woff');
+    font-weight: bolder;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'QSLight';
+    src: url('assets/fonts/quicksand-light-webfont.woff2') format('woff2'),
+         url('assets/fonts/quicksand-light-webfont.woff') format('woff');
+    font-weight: lighter;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'QSMed';
+    src: url('assets/fonts/quicksand-medium-webfont.woff2') format('woff2'),
+         url('assets/fonts/quicksand-medium-webfont.woff') format('woff');
+    font-weight: bold;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'QSReg';
+    src: url('assets/fonts/quicksand-regular-webfont.woff2') format('woff2'),
+         url('assets/fonts/quicksand-regular-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'QSReg', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -26,4 +62,35 @@
     }
   }
 }
+
+body {
+  margin: 0.5rem;
+}
+
+input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    font-family: 'QSReg', Arial, Helvetica, sans-serif;
+    border: 1px solid transparent;
+    border-radius: 3px;
+    background-color: #f1f1f1;
+    padding: 0.625rem;
+    font-size: 1rem;
+}
+input[type="submit"]{
+    font-family: 'QSMed', Arial, Helvetica, sans-serif;
+}
 </style>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'root',
+  components: {
+    Header
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,93 @@
+<template>
+  <header class="header-pokepage">
+    <div class="header-text">
+        <router-link to="/">
+            <img class="logo" src="../assets/images/pokesearch-icon.svg" alt="PokeSearch Logo">
+        </router-link>
+        <h1 class="site-name">PokéSearch</h1>
+    </div>
+
+    <div class="header-search">
+      <div class="search-box">
+        <input class="header-search-input" type="text" id="poke-search" name="p" value="" placeholder="Enter a Pokémon...">
+        <input class="header-search-submit" type="submit" id="submit" value="GO!">
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'header',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="css">
+header {
+    width: auto;
+    padding: 1rem;
+    /* margin: 0.2rem; */
+    /* margin-bottom: 0; */
+    background-color: #ef5350;
+    border-radius: 0.625rem;
+    color: white;
+
+    display: flex;
+    flex-direction: row;
+}
+
+.header-pokepage {
+    display: flex;
+    align-items: center;
+}
+.header-text {
+    flex-grow: 3;
+
+    display: flex;
+    align-items: center;
+}
+
+.logo {
+    height: 3rem;
+    margin-right: 1rem;
+}
+
+.site-name {
+    margin: 0;
+    display: none;
+    font-family: 'QSMed', Arial, Helvetica, sans-serif;
+}
+
+.header-search-input:hover {
+    border: 1px solid #707070;
+}
+.header-search-input:focus {
+    border: 1px solid #707070;
+}
+.header-search-submit { /* for poke_page only */
+    /* border: 1px solid white; */
+    background-color: #f1f1f1;
+    color: #ef4036;
+}
+.header-search-submit:hover { /* for poke_page only */
+    border: 1px solid #f1f1f1;
+    background-color: #4A4A4A;
+    color: #f1f1f1;
+}
+
+/* Styling for desktop/tablet viewing */
+@media screen and (min-width: 25.9375rem) {
+  header {
+      /* margin: 0.7rem;
+      margin-bottom: 0; */
+  }
+
+  .site-name {
+        margin: 0;
+        display: block;
+    }
+}
+</style>
