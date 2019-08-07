4954d0412046e6f7fbb124ed886ce05117f415a8
Joseph Lunine <jel99@drexel.edu>
2019-08-06 22:20:23 -0400

    Added pokeAPI call to get all names

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- {{ info }} -->
+    <div class="pokeBoxes">
+      <div class="pokeBox" v-bind:key="poke.id" v-for="poke in info">
+        {{ poke.name }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,6 +17,36 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios.get('https://pokeapi.co/api/v2/pokemon-species?&limit=9999')
+    .then(response => {
+      this.info = response.data.results;
+    });
   }
 }
 </script>
+
+<style lang="scss">
+
+  .pokeBoxes {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .pokeBox {
+    border: 2px solid #4A4A4A;
+    border-radius: 0.625rem;
+    padding: 0 1rem;
+    margin: 1rem 0;
+    text-align: left;
+  }
+
+</style>
+
