0a8cc7b9426b8f3d6198468d0e3f025101bfbd01
Joseph Lunine <jel99@drexel.edu>
2019-08-06 22:19:54 -0400

    Router view styling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
     <Header />
-    <router-view/>
+    <div class="router">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -52,15 +50,8 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.router {
+  margin: 0 1rem;
 }
 
 body {
