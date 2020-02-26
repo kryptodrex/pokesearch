d23e80d42774e4a35a3b679cb23c8f6357ecad47
Joseph <dingousa12@me.com>
2020-02-25 20:16:55 -0500

    added PokeBox component to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="home">
     <!-- {{ info }} -->
-    <div class="pokeBoxes">
-     
-        <div class="pokeBox" v-bind:key="poke.id" v-for="poke in pokeInfo">
-           <router-link :to="'/pokemon/' + poke.name">
-            {{ poke.name }}
-          </router-link>
-        </div>
+    <div class="pokeBoxes">  
+        <PokeBox v-for="poke in pokeInfo" v-bind:key="poke.id" :name="poke.name" />
     </div>
   </div>
 </template>
@@ -15,38 +10,38 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-  import { RepositoryFactory } from './../repositories/repositoryFactory';
-  const PokeRepository = RepositoryFactory.get('pokes');
-
-  export default {
-    name: 'Home',
-    // components: {
-    //   HelloWorld
-    // },
-    data() {
-      return {
-        isLoading: false,
-        pokeInfo: [],
-      };
-    },
-    created () {
-      this.fetch()
-    },
-    methods: {
-      async fetch () {
-        this.isLoading = true
-        const { data } = await PokeRepository.getAllPokemonSpecies()
-        console.log(data)
-        this.isLoading = false
-        this.pokeInfo = data.results;
-      }
-    },
-    computed: {
-      computedPosts () {
-        return this.posts.slice(0, 10)
-      }
+import { RepositoryFactory } from './../repositories/repositoryFactory'
+import PokeBox from '../components/PokeBox'
+const PokeRepository = RepositoryFactory.get('pokes')
+export default {
+  name: 'Home',
+  components: {
+    PokeBox
+  },
+  data () {
+    return {
+      isLoading: false,
+      pokeInfo: []
+    }
+  },
+  created () {
+    this.fetch()
+  },
+  methods: {
+    async fetch () {
+      this.isLoading = true
+      const { data } = await PokeRepository.getAllPokemonSpecies()
+      console.log(data)
+      this.isLoading = false
+      this.pokeInfo = data.results
+    }
+  },
+  computed: {
+    computedPosts () {
+      return this.posts.slice(0, 10)
     }
   }
+}
 </script>
 
 <style lang="scss">
@@ -56,13 +51,6 @@
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
-  }
-  .pokeBox {
-    border: 2px solid #4A4A4A;
-    border-radius: 0.625rem;
-    padding: 0 1rem;
-    margin: 1rem 0;
-    text-align: left;
   }
 
   a {
