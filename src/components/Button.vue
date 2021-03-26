1c89461d899128fb35d862a48d695ac6f3636944
Joseph Lunine <jel99@drexel.edu>
2021-03-25 22:05:20 -0400

    Create Button.vue

---
+++ src/components/Button.vue
@@ -0,0 +1,41 @@
+<template> 
+    <div class="actionBtn">
+      {{ text }}
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'PokeBox',
+  props: {
+    text: String
+  }
+}
+
+</script>
+
+
+<style>
+
+.actionBtn { /* for poke_page only */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #f1f1f1;
+    border: 1px solid #f1f1f1;
+    color: rgb(201, 38, 63);
+    font-weight: bold;
+    border-radius: 3px;
+    padding: 0.5rem;
+    cursor: pointer;
+}
+.actionBtn:hover { /* for poke_page only */
+    border: 1px solid #f1f1f1;
+    background-color: #4A4A4A;
+    color: #f1f1f1;
+    transition: 0.2s;
+}
+  
+</style>
