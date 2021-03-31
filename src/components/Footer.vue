c865f1b751b4468da7fefec62d4b90a5109c5fff
Joseph Lunine <jel99@drexel.edu>
2021-03-30 22:03:58 -0400

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <p>All images are owned by Pokémon. TM, ® Nintendo.</p>
+  </footer>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="css">
+footer {
+    width: auto;
+    margin: 0.7rem;
+    margin-top: 1rem;
+    border-radius: 0.625rem;
+    background-color: #4a4a4a;
+    color: white;
+    text-align: center;
+    bottom: 0;
+    /* margin: 0; */
+    padding: 1rem;
+}
+</style>
