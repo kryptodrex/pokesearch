0877b9512924c132f1bb827d912e52a49c21f7e5
Joseph Lunine <jel99@drexel.edu>
2021-04-02 19:10:21 -0400

    Create langRepository.js

---
+++ src/repositories/langRepository.js
@@ -0,0 +1,30 @@
+
+export default {
+    getBrowserLocales(options = {}) {
+        const defaultOptions = {
+            languageCodeOnly: false,
+        };
+
+        const opt = {
+            ...defaultOptions,
+            ...options,
+        };
+
+        const browserLocales =
+            navigator.languages === undefined
+            ? [navigator.language]
+            : navigator.languages;
+
+        if (!browserLocales) {
+            return undefined;
+        }
+
+        return browserLocales.map(locale => {
+            const trimmedLocale = locale.trim();
+
+            return opt.languageCodeOnly
+            ? trimmedLocale.split(/-|_/)[0]
+            : trimmedLocale;
+        });
+    }
+};
