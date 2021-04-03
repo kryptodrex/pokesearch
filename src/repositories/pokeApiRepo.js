4138b95997ec6480afaeeb21bc8db4d465fdab5e
Joseph Lunine <jel99@drexel.edu>
2021-04-03 00:01:19 -0400

    added error routing

--- src/repositories/pokeApiRepo.js
+++ src/repositories/pokeApiRepo.js
@@ -1,10 +1,22 @@
 import axios from "axios";
+import router from "@/router";
 
 // You can use your own logic to set your local or production domain
 const baseDomain = "https://pokeapi.co/api/v2";
 // The base URL is empty this time due we are using the jsonplaceholder API
 const baseURL = `${baseDomain}`;
 
-export default axios.create({
+const api = axios.create({
   baseURL
 });
+
+api.interceptors.response.use(null, error => {
+  let path = '/error';
+  switch (error.response.status) {
+    case 404: path = '/lost-in-the-tall-grass'; break;
+  }
+  router.push(path);
+  return Promise.reject(error);
+});
+
+export default api;
