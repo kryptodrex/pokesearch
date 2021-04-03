8e80f4bf1620291efb166032c2eb4f1ac621fa66
Joseph Lunine <jel99@drexel.edu>
2021-04-03 00:01:58 -0400

    Create NotFound.vue

---
+++ src/views/NotFound.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="gotLost">
+    <h2>404 Not Found</h2>
+    <p class="">The Pokémon you were searching for couldn't be found...</p>
+    <router-link :to="'/'">
+        <Button color="red" size="large" text="Head back home?" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+import router from '@/router';
+import Button from '@/components/Button';
+
+export default {
+  name: 'NotFound',
+  components: {
+      Button
+  },
+  created() {
+      this.getAlternatives();
+  },
+  methods: {
+      getAlternatives() {
+          var prevRoute = router;
+          console.log(prevRoute);
+      }
+  }
+
+}
+</script>
+
+<style scoped>
+
+.gotLost {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 80vh;
+}
+
+.pokemon-image {
+    margin-top: 1rem;
+    height: 10rem;
+}
+
+</style>
