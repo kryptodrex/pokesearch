a0fdcc5a33eca33500a64345b87dd1f225297bb7
Joseph Lunine <jel99@drexel.edu>
2021-04-04 14:53:45 -0400

    Create DexNavigation.vue

---
+++ src/components/DexNavigation.vue
@@ -0,0 +1,167 @@
+<template>
+    <div class="forward-back" v-if="!isLoading">
+        <div class="back-div">
+            <router-link :to="'/pokemon/' + prevNum">
+                <img class="arrow" id="back-arrow" src="@/assets/images/back.svg" alt="" v-on:click="navigatingTo(prevNum)">
+            </router-link>
+            <img class="pokesprite prev" :src="prevPokeData.spriteUrl" :alt="'Sprite of ' + prevPokeData.name">
+            <div class="pokenumname">
+                <div class="pokenum prev"> #{{ prevPokeData.dexNum }} </div>
+                <div class="pokename prev"> {{ prevPokeData.name }} </div>
+            </div>
+        </div>
+
+        <div class="forward-div">
+            <div class="spacer"></div>
+            <div class="pokenumname">
+                <div class="pokenum next"> #{{ nextPokeData.dexNum }} </div>
+                <div class="pokename next"> {{ nextPokeData.name }} </div>
+            </div>
+            <img class="pokesprite next" :src="nextPokeData.spriteUrl" :alt="'Sprite of ' + nextPokeData.name">
+            <router-link :to="'/pokemon/' + nextNum">
+                <img class="arrow" id="forward-arrow" src="@/assets/images/forward.svg" alt="" v-on:click="navigatingTo(nextNum)">
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import { RepositoryFactory } from '@/repositories/repositoryFactory';
+
+const pokeApi = RepositoryFactory.get('pokeApi');
+const util = RepositoryFactory.get('util');
+
+export default {
+    name: 'DexNavigation',
+    props: {
+        prevNum: null,
+        nextNum: null
+    },
+    data() {
+        return {
+            isLoading: false,
+            nextPokeData: null,
+            prevPokeData: null
+        }
+    },
+    created() {
+        this.fetch();
+    },
+    methods: {
+        async fetch() {
+            this.isLoading = true;
+
+            var { data } = await pokeApi.getPokemon(this.prevNum);
+            this.prevPokeData = {
+                name: util.toUpper(data.species.name),
+                dexNum: this.findIndex(this.prevNum),
+                spriteUrl: data.sprites.front_default
+            }
+
+            var { data } = await pokeApi.getPokemon(this.nextNum);
+            this.nextPokeData = {
+                name: util.toUpper(data.species.name),
+                dexNum: this.findIndex(this.nextNum),
+                spriteUrl: data.sprites.front_default
+            }
+
+            this.isLoading = false;
+        },
+
+        findIndex(value) {
+            return util.findIndex(value);
+        },
+
+        navigatingTo(value) {
+            this.$emit('navigate', value);
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="css">
+/* Next/Prev in Pokédex */
+.forward-back {
+    margin: 0 0.7rem;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto;
+    grid-template-areas: 'prev next';
+}
+
+.back-div, .forward-div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.back-div {
+    grid-area: prev;
+}
+
+.spacer {
+    flex-grow: 1;
+}
+.forward-div {
+    grid-area: next;
+    text-align: right;
+}
+
+.pokenumname {
+    display: flex;
+    flex-direction: column;
+}
+.prev {
+    text-align: left;
+    margin-left: 0.5rem;
+}
+
+/* .prev-num, .prev-name, .prev-sprite {
+    margin-left: 0.5rem;
+} */
+
+.pokesprite {
+    height: 2rem;
+}
+
+.next {
+    text-align: right;
+    margin-right: 0.5rem;
+}
+/* .next-num, .next-name, .next-sprite {
+    margin-right: 0.5rem;
+} */
+
+.arrow {
+    height: 1.875rem;
+    cursor: pointer;
+}
+
+.pokedex-num {
+    margin-bottom: 0;
+}
+
+/* Images */
+.pokemon-sprite {
+    margin-top: 1rem;
+}
+
+
+/* Viewing on smaller phones, like iPhone SE */
+@media screen and (max-width: 22.25rem) {
+    .prev-sprite, .pokesprite {
+        display: none;
+    }
+}
+
+/* Styling for desktop/tablet viewing */
+@media screen and (min-width: 25.9375rem) {
+    .pokenumname {
+        flex-direction: row;
+    }
+}
+
+</style>
