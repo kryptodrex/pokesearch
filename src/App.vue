7081e37589da2a5f16f40b5157c4495e33a99353
Joseph Lunine <jel99@drexel.edu>
2021-04-17 17:21:06 -0400

    added button class to app.vue

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@ export default {
 <style lang="scss">
 * {
   box-sizing: border-box;
+  outline: none;
   // cursor: url('assets/images/logo.svg'), auto !important;
 }
 
@@ -79,6 +80,16 @@ body {
   margin: 0.5rem;
 }
 
+.button {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+}
+
 input {
     -webkit-appearance: none;
     -moz-appearance: none;
@@ -92,10 +103,6 @@ input {
 }
 input[type="submit"]{
     font-family: 'QSMed', Arial, Helvetica, sans-serif;
-}
-
-summary {
-  outline: none;
 }
 
 a {
