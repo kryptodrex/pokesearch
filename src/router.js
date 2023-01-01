45ee6321310b0c2190a358f9bb3de119d531182c
Joseph Lunine <jel99@drexel.edu>
2023-01-01 16:36:50 -0500

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Pokemon from './views/Pokemon.vue'
-import NotFound from '@/views/NotFound.vue'
+import NotFound from './views/NotFound.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: import.meta.env.BASE_URL,
   routes: [
     {
       path: '/pokemon',
