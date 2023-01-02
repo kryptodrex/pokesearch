4d0e743fa83a4d2a5537484ab05eb69a6709e47b
Joseph Lunine <jel99@drexel.edu>
2023-01-02 09:56:34 -0500

    trying fixes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,64 @@
-import Vue from 'vue'
-import './plugins/axios'
+import { createApp } from '@vitejs/plugin-vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
 import App from './App.vue'
-import router from './router'
 import store from './store'
-// import titleMixin from './mixins/titleMixin'
 
-Vue.config.productionTip = false
-// Vue.mixin(titleMixin)
+import Home from '@/views/Home.vue'
+import Pokemon from '@/views/Pokemon.vue'
+import NotFound from '@/views/NotFound.vue'
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  store: store,
+  routes: [
+    {
+      path: '/pokemon',
+      name: 'homePokemon',
+      component: Home
+    },
+    {
+      path: '/pokemon/:name',
+      name: 'pokePageDirect',
+      component: Pokemon
+    },
+    {
+      path: '/pokemon/:name/forms',
+      name: 'pokePageForms',
+      component: Pokemon
+    },
+    {
+      path: '/pokemon/:name/forms/:form',
+      name: 'pokePageAltForm',
+      component: Pokemon
+    },
+    {
+      path: '/lost-in-the-tall-grass',
+      name: 'gotLost',
+      component: NotFound
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '*',
+      name: 'default',
+      component: Home
+    }
+  ]
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
+
+// Vue.config.productionTip = false
+// // Vue.mixin(titleMixin)
+
+// new Vue({
+//   router,
+//   store,
+//   render: h => h(App)
+// }).$mount('#app')
--- src/router.js
+++ src/router.js
@@ -1,14 +1,18 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Pokemon from './views/Pokemon.vue'
-import NotFound from './views/NotFound.vue'
+// import Vue from 'vue'
+// import Vue from '@vitejs/plugin-vue'
+// import { createApp } from '@vitejs/plugin-vue'
+import { createApp } from 'vue'
+// import Router from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
-Vue.use(Router)
+import Home from '@/views/Home.vue'
+import Pokemon from '@/views/Pokemon.vue'
+import NotFound from '@/views/NotFound.vue'
 
-export default new Router({
-  mode: 'history',
-  base: import.meta.env.BASE_URL,
+// Vue.use(Router)
+
+const router = createRouter({
+  history: createWebHistory(),
   routes: [
     {
       path: '/pokemon',
@@ -46,17 +50,63 @@ export default new Router({
       component: Home
     }
   ]
-  // scrollBehavior (to, from, savedPosition) {
-  //   if (to.hash) {
-  //     return {
-  //       selector: to.hash,
-  //       behavior: 'smooth'
-  //       // , offset: { x: 0, y: 10 }
-  //     }
-  //   } else if (savedPosition) {
-  //     return savedPosition
-  //   } else {
-  //     return { x: 0, y: 0 }
-  //   }
-  // }
 })
+
+createApp(App)
+  .use(router)
+  .mount('#app')
+
+// export default new Router({
+//   mode: 'history',
+//   base: import.meta.env.BASE_URL,
+//   routes: [
+//     {
+//       path: '/pokemon',
+//       name: 'homePokemon',
+//       component: Home
+//     },
+//     {
+//       path: '/pokemon/:name',
+//       name: 'pokePageDirect',
+//       component: Pokemon
+//     },
+//     {
+//       path: '/pokemon/:name/forms',
+//       name: 'pokePageForms',
+//       component: Pokemon
+//     },
+//     {
+//       path: '/pokemon/:name/forms/:form',
+//       name: 'pokePageAltForm',
+//       component: Pokemon
+//     },
+//     {
+//       path: '/lost-in-the-tall-grass',
+//       name: 'gotLost',
+//       component: NotFound
+//     },
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: Home
+//     },
+//     {
+//       path: '*',
+//       name: 'default',
+//       component: Home
+//     }
+//   ]
+//   // scrollBehavior (to, from, savedPosition) {
+//   //   if (to.hash) {
+//   //     return {
+//   //       selector: to.hash,
+//   //       behavior: 'smooth'
+//   //       // , offset: { x: 0, y: 10 }
+//   //     }
+//   //   } else if (savedPosition) {
+//   //     return savedPosition
+//   //   } else {
+//   //     return { x: 0, y: 0 }
+//   //   }
+//   // }
+// })
