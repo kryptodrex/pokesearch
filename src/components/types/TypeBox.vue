7d3b9fc9162ce8a97d5e307af971845997cd1a1d
Joseph <dingousa12@me.com>
2023-04-03 18:29:26 -0400

    updated type box to style properly

--- src/components/types/TypeBox.vue
+++ src/components/types/TypeBox.vue
@@ -1,22 +1,56 @@
 <template>
-    <div :class="'type-' + typeInfo.type.name" v-for="typeInfo in types" v-bind:key="typeInfo.slot">
-        {{ toUpper(typeInfo.type.name) }}
-    </div>
+    <router-link :to="'/types/' + typeName">
+        <div :class="'type-' + typeName + ' ' + sizeClass">
+            <slot></slot>
+        </div>
+    </router-link>
 </template>
 
 <script>
-
+export default {
+  name: 'TypeBox',
+  props: {
+    typeName: String,
+    size: String
+  },
+  computed: {
+    sizeClass () {
+      if (this.size !== null) return this.size
+      else return 'small'
+    }
+  }
+}
 </script>
 
-<style scopes lang="css">
+<style scoped lang="css">
+@import '../../styling/types.css';
+
 [class*="type-"] {
-    border-radius: 0.625rem;
+  border-radius: 0.625rem;
 
-    padding: 0.2rem 1rem;
-    margin: 0 0.5rem;
-    min-width: 5.625rem;
-    text-align: center;
+  padding: 0.2rem 1rem;
+  margin: 0 0.5rem;
+  min-width: 5.625rem;
+  text-align: center;
+}
+.clickable {
+  cursor: pointer;
+}
+.not-clickable {
+  cursor: text;
+}
 
-    /* cursor: pointer; */
+.large{
+  border-radius: 0.5rem;
+  font-size: 1.2rem;
+  padding: 0.8rem 1.5rem;
+}
+.medium {
+  border-radius: 0.4rem;
+  font-size: 1rem;
+  padding: 0.5rem 0.8rem;
+}
+.small {
+  /* font-size: 1rem; */
 }
 </style>
