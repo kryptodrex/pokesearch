c9c9dcbd2bae3dbf7fa51b31dd87c2298af1025a
Joseph <dingousa12@me.com>
2023-04-16 14:22:47 -0400

    Update TypeBox.vue

--- src/components/types/TypeBox.vue
+++ src/components/types/TypeBox.vue
@@ -1,9 +1,16 @@
 <template>
-    <router-link :to="'/types/' + typeName">
-        <div :class="'type-color-' + typeName + ' ' + 'type-border-' + typeName + ' ' + sizeClass">
-            <slot></slot>
+    <div v-on:click="toggleSelect()">
+        <router-link v-if="enableLink" :to="'/types/' + typeName">
+            <div :class="'typeLink type-color-' + typeName + ' ' + ' type-border-' + typeName + ' type-btn-' + typeName + ' ' + sizeClass + ' ' + selectedClass">
+                <slot></slot>
+            </div>
+        </router-link>
+        <div v-if="!enableLink">
+            <div :class="'type-color-' + typeName + ' ' + ' type-border-' + typeName + ' ' + sizeClass + ' ' + clickableClass + ' ' + selectedClass">
+                <slot></slot>
+            </div>
         </div>
-    </router-link>
+    </div>
 </template>
 
 <script>
@@ -11,19 +18,44 @@ export default {
   name: 'TypeBox',
   props: {
     typeName: String,
-    size: String
+    size: String,
+    linkEnabled: Boolean,
+    clickable: Boolean
+  },
+  data () {
+    return {
+      selected: false
+    }
+  },
+  methods: {
+    toggleSelect () {
+      if (this.selected) this.selected = false
+      else this.selected = true
+    }
   },
   computed: {
     sizeClass () {
       if (this.size !== null) return this.size
       else return 'small'
+    },
+    enableLink () {
+      if (this.linkEnabled !== null) return this.linkEnabled
+      else return true
+    },
+    clickableClass () {
+      if (this.clickable) return 'type-btn-' + this.typeName
+      else return ''
+    },
+    selectedClass () {
+      if (this.selected) return 'selected'
+      else return ''
     }
   }
 }
 </script>
 
-<style scoped lang="css">
-@import '../../styling/types.css';
+<style scoped lang="scss">
+@import '../../styling/types.scss';
 
 [class*="type-"] {
   border-radius: 0.625rem;
@@ -50,4 +82,13 @@ export default {
 /* .small {
   font-size: 1rem;
 } */
+
+// .typeLink {
+//     transition: 0.2s;
+// }
+// .typeLink:hover, .typeLink:focus {
+//   box-shadow: 0 4px 4px 0 rgba(0,0,0,0.20);
+//   transition: 0.3s;
+// }
+
 </style>
