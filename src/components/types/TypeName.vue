ae378376ae2baafd05d3fc9de4c4144d164fadca
Joseph <dingousa12@me.com>
2023-04-17 18:16:58 -0400

    Create TypeName.vue

---
+++ src/components/types/TypeName.vue
@@ -0,0 +1,18 @@
+<template>
+    <span ref="wrapper" :class="'type-color-' + name">
+        <b><slot></slot></b>
+    </span>
+</template>
+
+<script>
+export default {
+  name: 'TypeName',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../styling/types.scss';
+</style>
